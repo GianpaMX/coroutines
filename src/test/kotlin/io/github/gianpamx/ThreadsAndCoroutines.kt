9d8ad9cc69722dec414680709cfb7692cb5310c0
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2018-05-18 15:09:24 -0500

    A lot of threads

--- src/test/kotlin/io/github/gianpamx/ThreadsAndCoroutines.kt
+++ src/test/kotlin/io/github/gianpamx/ThreadsAndCoroutines.kt
@@ -3,9 +3,12 @@ package io.github.gianpamx
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.thread
 
 @RunWith(JUnit4::class)
 class ThreadsAndCoroutines {
@@ -37,5 +40,18 @@ class ThreadsAndCoroutines {
             delay(2000) // wait for 2 seconds
         }
         println("Stop")
+    }
+
+    @Ignore("My patience runs out")
+    @Test(timeout = 5_000)
+    fun aLotOfThreads() {
+        val c = AtomicInteger()
+
+        for (i in 1..1_000_000)
+            thread(start = true) {
+                c.addAndGet(i)
+            }
+
+        println(c.get())
     }
 }

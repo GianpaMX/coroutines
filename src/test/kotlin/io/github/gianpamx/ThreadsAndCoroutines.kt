578f7e4fd72c3bb58dabde53e887772719171269
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2018-05-18 15:06:04 -0500

    Blocking instead of thread sleep

--- src/test/kotlin/io/github/gianpamx/ThreadsAndCoroutines.kt
+++ src/test/kotlin/io/github/gianpamx/ThreadsAndCoroutines.kt
@@ -2,6 +2,7 @@ package io.github.gianpamx
 
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -19,6 +20,22 @@ class ThreadsAndCoroutines {
         }
 
         Thread.sleep(2000) // wait for 2 seconds
+        println("Stop")
+    }
+
+    @Test
+    fun blockingInsteadOfThreadSleep() {
+        println("Start")
+
+        // Start a coroutine
+        launch {
+            delay(1000)
+            println("Hello")
+        }
+
+        runBlocking {
+            delay(2000) // wait for 2 seconds
+        }
         println("Stop")
     }
 }

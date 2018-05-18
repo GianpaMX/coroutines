a00033e4968d0fc62043e994e7493bf2bef8e2df
Juan Pablo "Gianpa" Villa <juan64@gmail.com>
2018-05-18 15:05:10 -0500

    My first coroutine

--- src/test/kotlin/io/github/gianpamx/ThreadsAndCoroutines.kt
+++ src/test/kotlin/io/github/gianpamx/ThreadsAndCoroutines.kt
@@ -1,5 +1,7 @@
 package io.github.gianpamx
 
+import kotlinx.coroutines.experimental.delay
+import kotlinx.coroutines.experimental.launch
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
@@ -7,7 +9,16 @@ import org.junit.runners.JUnit4
 @RunWith(JUnit4::class)
 class ThreadsAndCoroutines {
     @Test
-    fun empty() {
-        
+    fun myFirstCoroutine() {
+        println("Start")
+
+        // Start a coroutine
+        launch {
+            delay(1000)
+            println("Hello")
+        }
+
+        Thread.sleep(2000) // wait for 2 seconds
+        println("Stop")
     }
 }
